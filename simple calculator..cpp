d5c68411d31ef545d169ce403a621c29339247f4
QudsiaKhan <124254768+QudsiaKhan@users.noreply.github.com>
2023-02-10 14:37:29 +0500

    Add files via upload

---
+++ simple calculator..cpp
@@ -0,0 +1,17 @@
+#include<iostream>//header file 
+using namespace std;//library file 
+
+int main()//main function 
+{//opening curly bracket 
+ double a,b;//declare variables with data type of double 
+ cout<<"Enter 1st number";//to print the statement 
+ cin>>a;//to get input from user 
+ cout<<"Enter 2nd number";//to print the statement 
+ cin>>b;//to get input from user 
+ 
+ cout<<a<<"+"<<b<<"="<<a+b<<endl;//to print the result 
+ cout<<a<<"-"<<b<<"="<<a-b<<endl;//to print the result 
+ cout<<a<<"*"<<b<<"="<<a*b<<endl;//to print the result 
+ cout<<a<<"/"<<b<<"="<<a/b;//to print the result 
+    return 0;//closing main function 
+}//closing curly bracket 

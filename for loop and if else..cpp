1305e91cbd881eaad3cca3e30d971973742ca6d8
QudsiaKhan <124254768+QudsiaKhan@users.noreply.github.com>
2023-02-23 15:36:56 +0500

    Add files via upload

---
+++ for loop and if else..cpp
@@ -0,0 +1,25 @@
+// if given num is less than 5, it's print name otherwise roll No.
+#include<iostream>//header file 
+using namespace std;//library file 
+
+int main()//main function 
+{//opening curly bracket 
+ int a,x;//declaring variables
+  cout<<"Enter a number: ";//to print the statement 
+ cin>>a;//to get input from user 
+ if(a<=5)//condition 
+{//opening if structure 
+ for(x=1;x<=5;x++)//for loop
+ {//opening loop body 1
+ cout<<"QudsiaKhan"<<endl;//to print the result 
+ }//closing loop body 1
+}//closing if structure 
+ else//condition 
+{//opening else structure 
+ for(x=1;x<=5;x++)//for loop
+ {//opening loop body 2
+ cout<<"Bsf2205806"<<endl;//to print the result 
+ }//closing loop body 2 
+}//closing else structure 
+    return 0;//closing main function 
+}//closing curly bracket 

8b839b97b94b1a08bf46082b226c44a217f34480
QudsiaKhan <124254768+QudsiaKhan@users.noreply.github.com>
2023-02-23 15:37:32 +0500

    Add files via upload

---
+++ counting in do while loop..cpp
@@ -0,0 +1,16 @@
+#include<iostream>//header file 
+using namespace std;//library file 
+
+int main()//main function 
+{ //opening curly bracket 
+ int a=1,b;//declare a variable 
+ cout<<"Enter a num till you want to write counting: ";//to print the statement 
+ cin>>b;//to get input from user 
+ do//loop structure 
+ {//opening loop body
+ cout<<a<<"\n";//to print the result 
+ a++;//increment 
+ }//closing loop body 
+ while(a<=b);//condition 
+    return 0;//closing main function 
+}//closing curly bracket 

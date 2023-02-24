480e1b93fb3043a21fcfdf611f06e5769668c0c1
QudsiaKhan <124254768+QudsiaKhan@users.noreply.github.com>
2023-02-24 15:30:18 +0500

    Add files via upload

---
+++ sum and mean..cpp
@@ -0,0 +1,16 @@
+#include<iostream>//header file 
+using namespace std;//library file 
+
+int main()//main function 
+{//opening curly bracket 
+ float a,b,sum,mean;//
+ cout<<"Enter 1st number";//to print the statement 
+ cin>>a;//to get input from user 
+ cout<<"Enter 2nd number";//to print the statement 
+ cin>>b;//to get input from user 
+ sum=a+b;//sum function 
+ cout<<"Sum is "<<sum<<endl;//to print the result 
+ mean=sum/2;//mean function 
+ cout<<"Mean is "<<mean;//to print the result 
+    return 0;//closing main function 
+}//closing curly bracket 
